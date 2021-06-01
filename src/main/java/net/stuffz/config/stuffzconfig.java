84bdd6570c41cd6d7b6cf70673bf398f20e74d5d
Globox1997 <mitschke11@web.de>
2021-06-01 16:42:41 +0200

    flush

--- src/main/java/net/stuffz/config/stuffzconfig.java
+++ src/main/java/net/stuffz/config/stuffzconfig.java
@@ -7,7 +7,6 @@ import me.sargunvohra.mcmods.autoconfig1u.shadowed.blue.endless.jankson.Comment;
 @Config(name = "stuffz")
 @Config.Gui.Background("minecraft:textures/block/stone.png")
 public class StuffzConfig implements ConfigData {
-  // @ConfigEntry.Gui.PrefixText
   public boolean generate_bushes = true;
   public boolean generate_geysers = true;
   @Comment("Disabling ore generating needs a restart")
