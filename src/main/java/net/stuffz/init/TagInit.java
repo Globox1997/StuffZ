a667ac0b30e7c03c48df881610c0abc5dd170a30
Globox1997 <mitschke11@web.de>
2021-10-06 12:50:56 +0200

    updated dependencies

--- src/main/java/net/stuffz/init/TagInit.java
+++ src/main/java/net/stuffz/init/TagInit.java
@@ -1,13 +1,13 @@
 package net.stuffz.init;
 
-import net.fabricmc.fabric.api.tag.TagRegistry;
+import net.fabricmc.fabric.api.tag.TagFactory;
 import net.minecraft.item.Item;
 import net.minecraft.tag.Tag;
 import net.minecraft.util.Identifier;
 
 public class TagInit {
 
-    public static final Tag<Item> AXE_ITEMS = TagRegistry.item(new Identifier("stuffz", "axe_items"));
+    public static final Tag<Item> AXE_ITEMS = TagFactory.ITEM.create(new Identifier("stuffz", "axe_items"));
 
     public static void init() {
     }
